39aa945643502a3be2882c5cc1b7c8c1e12d278b
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2014-03-30 11:35:49 +0900

    fix: modules

--- modules/essentials/manifests/install.pp
+++ modules/essentials/manifests/install.pp
@@ -7,7 +7,7 @@ class essentials::install {
     'libxslt1.1', 'libxslt-dev', 'libxml2', 'libxml2-dev', 'libssl-dev', 'libffi-dev', 'libsqlite3-dev', 'libreadline6-dev', 'libgd2-xpm-dev',
 #    'libiconv', 'libiconv-dev',
     'libyaml-dev', 'sqlite3', 'libgdbm-dev', 'libncurses5-dev', 'libtool',
-    'git-core', 'subversion', 'mercurial'
+    'git-core', 'subversion', 'mercurial',
     'lv',
     'unzip',
     'flex', 'bison',
